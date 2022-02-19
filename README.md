2d6d436b20eeaf157a64a60156ea01e036a25c8d
dymafr <erwan1309@hotmail.com>
2022-02-19 12:20:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-lj7frq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-lj7frq)
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 @click="userClick.left.once">Bonjour tout le monde !</h1>
+
+  <form @submit.prevent="formSubmit">
+    <button>Envoyer</button>
+  </form>
+
+  <input type="text" @keyup.enter="handleEnter()" />
+</template>
+
+<script setup lang="ts">
+function userClick(event: MouseEvent) {
+  console.log(event);
+}
+function formSubmit() {
+  console.log('Envoyé');
+}
+function handleEnter() {
+  console.log('Entrée');
+}
+</script>
+
+<style></style>
